46c028363a028a621fba199e382942f3c1d4880e
lucasoboli <lucasoboli@gmail.com>
2020-05-24 15:31:11 -0300

    Adjustment to NoMatch

--- web-app/src/components/NoMatch.js
+++ web-app/src/components/NoMatch.js
@@ -1,50 +1,20 @@
 import React from 'react';
-//import $ from 'jquery';
+import $ from 'jquery';
 import '../style/NoMatch.css';
 
 
 class NoMatch extends React.Component {
-    /*componentDidMount() {
-        var formatThousandsNoRounding = function (n,dp) {
-            var e='', s=e+n, l=s.length, b=n<0 ? 1 : 0,
-                i=s.lastIndexOf(','), j=i===-1 ? l : i,
-                r=e, d=s.substr(j+1,dp);
-            
-            while ((j-=3) > b) {
-                r = '.' + s.substr(j,3) + r;
-            }
-
-            return s.substr(0,j+3) + r + 
-                (dp ? ',' + d + (d.length < dp ? ('00000').substr(0, dp-d.length):e):e);
-        };
-
-        var hasRun = false;
-
-        inView('#countUp').on('enter', function() {
-            if(hasRun === false) {
-                $('.number').each(function() {
-                    var $this = $(this),
-                    countTo = $this.attr('data-count');
-                
-                    $({ countNum: $this.text()}).animate({
-                        countNum: countTo
-                    },
-                    {
-                        duration: 2000,
-                        easing:'swing',
-                        step: function() {
-                            $this.text(formatThousandsNoRounding(Math.floor(this.countNum)));
-                        },
-                        complete: function() {
-                            $this.text(formatThousandsNoRounding(this.countNum));
-                        }
-                    });
-                });
-
-                hasRun = true;
-            }
-        });
-    }*/
+    componentDidMount() {
+        /*
+        var container = document.getElementById('nm-body');
+        window.onmousemove = function(e) {
+            var x = e.clientX/10,
+                y = e.clientY/10;
+            container.style.backgroundPositionX = x + 'px';
+            container.style.backgorundPositionY = y + 'py';
+        }
+            */
+    }
 
 
     render() {
