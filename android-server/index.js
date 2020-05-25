3202a791f393e0169bccdb6fe9763388fcbf04f5
Geowana Siqueira <geowana@motorola.com>
2020-05-24 22:27:07 -0300

    add o qr code atualizando e as rotas do app

--- android-server/index.js
+++ android-server/index.js
@@ -1,27 +1,26 @@
 //npm i -y
 //npm install express --save
-//npm install body-parser --save
-//npm install qr-image --save
 //npm install crypto-js --save
-//npm install parse-json --save
+//npm install body-parser --save
 
-var express = require("express");
-var app = express();
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 8080;
 var bodyParser = require('body-parser');
-const qr = require('qr-image');
-
-
-app.listen(8080, () => {
-    console.log("Server running on port 8080");
-});
 
 app.use(bodyParser.json());
 
-//Url that receive the number of phone
+const {getTime} = require('./utils/time');
+const {qrgen} = require('./utils/qrCode');
+
+//-----------------------------------------------------------------------------------------------------
+// 1 - Rota para login do aplicativo
+//-----------------------------------------------------------------------------------------------------
+
 app.post('/authentification', function (req, res, next) {
-
-    let { numberPhone } = req.body;
-
+    let responseAuth = req.body;
+    let {numberPhone} = req.body;
     console.log(numberPhone);
 
     /**********************************************************************************************
@@ -29,7 +28,7 @@ app.post('/authentification', function (req, res, next) {
      //req.body = { numberPhone: '+5535979999887' }
      //numberPhone = 359998859201
 
-     Pegar no banco a matricula do aluno vinculada ao número de celular que está na numberPhone
+     Buscar no banco a matricula do aluno vinculada ao número de celular que está na numberPhone
 
      | ID | MATRICULA | NUM_CELULAR|
      | 1  | 34191     | 3598859201 |
@@ -39,89 +38,86 @@ app.post('/authentification', function (req, res, next) {
      | 5  | 22588     | 3598859205 |
 
      Retornar a matricula e colocar na variavel registration
-    **********************************************************************************************/
+     **********************************************************************************************/
 
     const registration = "34191";
-
-    //montar um json de retorno com a matricula
-
-    let responseAuth = req.body;
     responseAuth.registration = registration;
-
-    // responseAuth = { numberPhone: '+5576676767677', registration: '34191' }
-
+    // responseAuth = { numberPhone: '+5576676767677', registration: '34191'}
     res.json(responseAuth)
 })
 
-//----------------------------------------------------------------------------------------------------
-// UnixTime NTP através da API Fonte: http://worldtimeapi.org/
-//----------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------
+// 2 - Rota para qr Code na tela
+//-----------------------------------------------------------------------------------------------------
 
-const request = require('request');
-const hostname = `http://worldtimeapi.org/api/timezone/America`;
-const path = `/Cuiaba`;
-request(`${hostname}${path}`, (err, res, body) => {
-    let {unixtime} = JSON.parse(res.body);
-   console.log(unixtime);
-
-/*
-    TO-DO
-    somar + 10s no unixtime 
-
-*/
-
-//----------------------------------------------------------------------------------------------------
-// Construindo a Url de dados para o QrCode
-//----------------------------------------------------------------------------------------------------
-
-/*
-    TO-DO
-    Pegar o initials e o class do banco 
-*/
-
-    url = `{'initials': 'eco', 'class': 't1', 'unixTime': ${unixtime}}`;
-
-//----------------------------------------------------------------------------------------------------
-// Encriptando a url para o QrCode
-//----------------------------------------------------------------------------------------------------
-
-    var crypto = require('crypto')
-    var password = "universidade-federal-de-itajuba-2020";
-    var cipher = crypto.createCipher('aes-128-ecb', password)
-    var text = "the big brown fox jumped over the fence"
-    var crypted = cipher.update(url, 'utf-8', 'hex')
-    crypted += cipher.final('hex')
-
-    console.log(crypted);
-//now crypted contains the hex representation of the ciphertext
-
-//----------------------------------------------------------------------------------------------------
-//Gerador de QR code no formato de imagem
-//----------------------------------------------------------------------------------------------------
-    app.set('view engine', 'ejs');
-    app.get('/qr', (req, res) => res.render('home'))
-
-    app.get('/qrcode', (req, res) => {
-        const code = qr.image(crypted, {type: 'png'})
-        res.type('png')
-        code.pipe(res)
-    })
-
-    //Para ver o qrCode localhost:8080/qrCode
+app.get('/qrcode', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-/*****************************************************************************************************
- * TO-DO
- * celular vai enviar um json:
- * {'initials': 'eco', 'class': 't1', 'unixTime': unixTime, 'Registration' : '34191' }
- *
- * usar essas dados para dar a presença ao aluno
- *
- */
+async function qrCode() {
+    try {
+        //Horário utc do servidor
+        const unixtime = await getTime(`http://worldtimeapi.org/api/timezone/America/Cuiaba`);
+        const _rangeUnixtime = unixtime + 15;
+        /**********************************************************************************************
+         TO-DO: Buscar no banco as informações de _acronyms e _class (infos da aula)
+         **********************************************************************************************/
+        var _acronyms = 'eco';
+        var _class = 't1';
+        //token do QrCode 
+        token = `{'_acronyms': ${_acronyms}, '_class': ${_class}, 'unixTime': ${_rangeUnixtime}}`;
+        console.log(token);
+
+        //criptografia do token 
+        var crypto = require('crypto')
+        var password = "universidade-federal-de-itajuba-2020";
+        var cipher = crypto.createCipher('aes-128-ecb', password)
+        var text = "the big brown fox jumped over the fence"
+        var crypted = cipher.update(token, 'utf-8', 'hex')
+        crypted += cipher.final('hex')
+
+        //gerando a url com a respectiva imagem do qr code com o token acima
+        qrgen(`${crypted}`).then(data => {
+            console.log(data);
+            io.emit('chat message', `${data}`);
+        }).catch(error => {
+            console.log(error);
+        });
+
+    } catch (e) {
+        throw e;
+    }
+}
+
+setInterval(async () => {
+    qrCode();
+}, 15000);
+
+//-----------------------------------------------------------------------------------------------------
+// 3 - Rota para qr Code na tela
+//-----------------------------------------------------------------------------------------------------
+
 app.post('/presence', function (req, res, next) {
 
-    //req.body = {'initials': 'eco', 'class': 't1', 'unixTime': unixTime, 'Registration' : '34191' }
     console.log(req.body);
-    res.status(200);
+    /*****************************************************************************************************
+     * TO-DO
+     * - a requisição:
+     {
+                        _acronyms: 'eco',
+                        _class: 't1',
+                        unixTimeQr: '1590368807',
+                        unixTimeApp: '1590368797',
+                        registration: '34191'
+                    }
 
+     * - usar a requisição acima  para efetuar a presença do aluno
+     *****************************************************************************************************/
+    response = {code: 200};
+    //envia o json com código 200 de sucesso
+    res.json(response);
+});
+
+http.listen(port, function () {
+    console.log('listening on *:' + port);
 });
